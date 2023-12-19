32a06cc801ff57e89ee300e71628db903f3d28ee
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-12-19 16:20:40 +0000

    create delete and add task function and save data.

--- script.js
+++ script.js
@@ -1,55 +1,40 @@
-const ITEMS_CONTAINER = document.getElementById("items");
-const ITEM_TEMPLATE = document.getElementById("itemTemplate");
-const ADD_BUTTON = document.getElementById("add");
+const inputBox = document.getElementById("input-box");
+const listContainer= document.getElementById("list-container");
 
 
-let  items = getItems();
-
-function getItems(){
-    const value = localStorage.getItem("todo-test") || "[]";
-
-    return JSON.parse(value):
-
-}
-
-function setItems(items) {
-    const itemsJson = JSON.stringify(items);
-    local.Storage.setItems("todo-test", itemsJson);
-
-}
-
-function addItem(){
-    items.unshift({
-        desciption:"",
-        completed: false
-    });
-    setItems(items);
-    refreshList();
-}
-
-function refreshList(){
-    //TODO sort items
-
-    ITEMS_CONTAINER.innerHTML = "";
-
-    for(const item of items) {
-        const itemElement = ITEM_TEMPLATE.contentEditable.cloneNode(true);
-        const descriptionInput = itemElement.querySelector(".item-description");
-        const completedInput = itemElement.querySelector(".item-completed");
-
-        descriptionInput.value = item.description;
-        completedInput.checked = item.completed;
-
-        ITEMS_CONTAINER.append(itemElement);
-
-
+function addTask(){
+    if (inputBox.value === ''){
+        alert("You must write something!");
     }
+ else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+}
+inputBox.value = '';
+saveData();
 }
 
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
 
-ADD_BUTTON.addEventListener("click", () =>{
-    addItem();
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
 
-});
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
 
-refreshList();
+}
+showTask();
